dab9b3a0e70697ce107eb02a05b955787cfd32a3
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-07-02 09:10:33 -0500

    Added dodge Queer Bubble

--- Plugins/Trust/Dungeons/BrayfloxsLongstop.cs
+++ Plugins/Trust/Dungeons/BrayfloxsLongstop.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using ff14bot;
+using ff14bot.Managers;
+using ff14bot.Objects;
+using System.Linq;
+using System.Threading.Tasks;
 using Trust.Data;
 
 namespace Trust.Dungeons
@@ -13,12 +17,31 @@ namespace Trust.Dungeons
         /// </summary>
         public new const ZoneId ZoneId = Data.ZoneId.BrayfloxsLongstop;
 
+        private const int GreatYellowPelican = 1280;
+        private const int InfernoDrake = 1284;
+        private const int Hellbender = 1286;
+        private const int bubbleObj = 1383;
+        private const int Aiatar = 1279;
+
+
         /// <inheritdoc/>
         public override DungeonId DungeonId => DungeonId.BrayfloxsLongstop;
 
         /// <inheritdoc/>
         public override async Task<bool> RunAsync()
         {
+            BattleCharacter hellbenderNpc = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(NpcId: Hellbender)
+                .FirstOrDefault(bc => bc.Distance() < 50 && bc.IsTargetable);
+            if (hellbenderNpc != null && hellbenderNpc.IsValid)
+            {
+                BattleCharacter bubbleNpc = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(NpcId: bubbleObj)
+                    .FirstOrDefault(bc => bc.Distance() < 50 && bc.IsVisible);
+                if (bubbleNpc != null && bubbleNpc.IsValid)
+                {
+                    AvoidanceManager.AddAvoidObject<GameObject>(() => Core.Player.InCombat, 2f, bubbleNpc.ObjectId);
+                }
+            }
+
             return false;
         }
     }
