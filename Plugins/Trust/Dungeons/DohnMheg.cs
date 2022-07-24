797411c01578688705204fe31527dd3e9262cccd
Manta <manta@bismuth.dev>
2022-07-24 13:12:50 -0400

    Dohn Mheg: Complete rework; resolves boss 2 tethers

--- Plugins/Trust/Dungeons/DohnMheg.cs
+++ Plugins/Trust/Dungeons/DohnMheg.cs
@@ -1,11 +1,14 @@
 using Buddy.Coroutines;
 using Clio.Utilities;
 using ff14bot;
+using ff14bot.Behavior;
 using ff14bot.Helpers;
 using ff14bot.Managers;
 using ff14bot.Navigation;
 using ff14bot.Objects;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Media;
@@ -25,214 +28,192 @@ namespace Trust.Dungeons
         /// </summary>
         public new const ZoneId ZoneId = Data.ZoneId.DohnMheg;
 
+        private const int LordOfLingeringGaze = 8141;
+        private const int Griaule = 8143;
+        private const int PaintedSapling = 8144;
+        private const int LordOfLengthsomeGait = 8146;
+        private const int LiarsLyre = 8958;
+
+        private const int ImpChoir = 13552;
+        private const int Finale = 15723;
+
+        private const int LaughingLeapDuration = 30_000;
+        private const int FodderDuration = 13_000;
+
+        private readonly Stopwatch laughingLeapSw = new();
+
+        private readonly HashSet<uint> spellsDodgedByFollowingClosest = new()
+        {
+            13551, 13547, 13952,
+        };
+
+        private readonly HashSet<uint> laughingLeap = new()
+        {
+            8852, 8840,
+        };
+
+        private readonly List<Vector3> tightRopeWalkPoints = new()
+        {
+            new Vector3(-142.8355f, -144.5264f, -232.6624f),
+            new Vector3(-140.8284f, -144.5366f, -246.1443f),
+            new Vector3(-130.1889f, -144.5366f, -242.3840f),
+            new Vector3(-114.4550f, -144.5366f, -244.2632f),
+            new Vector3(-125.6857f, -144.5238f, -249.2640f),
+            new Vector3(-122.5055f, -144.5192f, -258.3726f),
+            new Vector3(-128.1084f, -144.5226f, -258.0896f),
+        };
+
+        private DateTime laughingLeapEnds = DateTime.MinValue;
+
+        private DateTime fodderEnds = DateTime.MinValue;
+        private Vector3 fodderTetherPoint = Vector3.Zero;
+
         /// <inheritdoc/>
         public override DungeonId DungeonId => DungeonId.DohnMheg;
 
         /// <inheritdoc/>
         public override async Task<bool> RunAsync()
         {
-            PluginContainer sidestepPlugin = PluginHelpers.GetSideStepPlugin();
+            await FollowDodgeEnemySpells();
 
-            IEnumerable<BattleCharacter> check = GameObjectManager.GetObjectsOfType<BattleCharacter>().Where(r => r.CastingSpellId != 0 && !r.IsMe && r.Distance() < 50 && (r.CastingSpellId == 15723 || r.CastingSpellId == 13520 || r.CastingSpellId == 13844));
+            SubZoneId currentSubZoneId = (SubZoneId)WorldManager.SubZoneId;
+            bool result = false;
 
-            IEnumerable<BattleCharacter> check2 = GameObjectManager.GetObjectsOfType<BattleCharacter>().Where(r => r.CastingSpellId != 0 && !r.IsMe && r.Distance() < 50 && (r.CastingSpellId == 13552));
-
-            IEnumerable<BattleCharacter> sC3 = GameObjectManager.GetObjectsOfType<BattleCharacter>().Where(r => !r.IsMe && r.Distance() < 50 && r.NpcId == 8146);  // 73BOSS3
-
-            // 过独木桥 BOSS 3
-            if (check.Any() == true && sC3.First().Location.Distance2D(Core.Me.Location) >= 10)
+            switch (currentSubZoneId)
             {
-                // 读条中断
-                if (Core.Me.IsCasting)
+                case SubZoneId.TeagGye:
+                    result = await HandleLordOfLingeringGazeAsync();
+                    break;
+                case SubZoneId.TheAtelier:
+                    result = await HandleGriauleAsync();
+                    break;
+                case SubZoneId.TheThroneRoom:
+                    result = await HandleLordOfLengthsomeGaitAsync();
+                    break;
+            }
+
+            return result;
+        }
+
+        private async Task<bool> FollowDodgeEnemySpells()
+        {
+            BattleCharacter caster = GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false)
+                .FirstOrDefault(bc => spellsDodgedByFollowingClosest.Contains(bc.CastingSpellId) && bc.Distance() < 50);
+
+            if (caster != null)
+            {
+                SpellCastInfo spell = caster.SpellCastInfo;
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, spell.RemainingCastTime, $"Follow-Dodge: ({caster.NpcId}) {caster.Name} is casting ({spell.ActionId}) {spell.Name} for {spell.RemainingCastTime.TotalMilliseconds:N0}ms");
+                await MovementHelpers.GetClosestAlly.Follow();
+            }
+
+            return false;
+        }
+
+        private async Task<bool> HandleLordOfLingeringGazeAsync()
+        {
+            BattleCharacter boss1 = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(LordOfLingeringGaze)
+                .FirstOrDefault(bc => bc.IsTargetable && bc.Distance() < 50);
+
+            if (laughingLeap.IsCasting() && laughingLeapEnds < DateTime.Now)
+            {
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, LaughingLeapDuration, $"Dodging Laughing Leap / Landsblood geysers");
+                laughingLeapEnds = DateTime.Now.AddMilliseconds(LaughingLeapDuration);
+            }
+
+            if (DateTime.Now < laughingLeapEnds)
+            {
+                // Intentionally follow tank to find melee uptime since no cleave damage is occurring
+                await MovementHelpers.GetClosestTank.FollowTimed(laughingLeapSw, LaughingLeapDuration);
+            }
+
+            return false;
+        }
+
+        private async Task<bool> HandleGriauleAsync()
+        {
+            BattleCharacter boss2 = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(Griaule)
+                .FirstOrDefault(bc => bc.IsTargetable && bc.Distance() < 50);
+
+            if (boss2 != null)
+            {
+                // Saplings change target in real-time to whomever's currently blocking their tether.
+                // Since there are always 5 saplings + 3 Trust members, wait for them to pick their tethers,
+                // then try to block the closest remaining tether.
+                IEnumerable<BattleCharacter> unhandledSaplings = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(PaintedSapling)
+                    .Where(bc => bc.CurrentTargetId == boss2.ObjectId);
+
+                if (unhandledSaplings.Count() == 2 && fodderEnds < DateTime.Now)
+                {
+                    CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, FodderDuration, $"Blocking sapling tether");
+                    fodderEnds = DateTime.Now.AddMilliseconds(FodderDuration);
+
+                    BattleCharacter sapling = unhandledSaplings.OrderBy(bc => bc.Distance()).First();
+
+                    Vector3 start = boss2.Location;
+                    Vector3 end = sapling.Location;
+                    fodderTetherPoint = start.GetPointBetween(end, 8.0f);
+
+                    Logging.Write(Colors.Aquamarine, $"Blocking sapling tether: {start} <- {start.Distance(fodderTetherPoint):N2} -> {fodderTetherPoint} <- {fodderTetherPoint.Distance(end):N2} -> {end}");
+                }
+
+                if (DateTime.Now < fodderEnds && Core.Player.Distance(fodderTetherPoint) > 0.5f)
+                {
+                    await CommonTasks.MoveTo(fodderTetherPoint);
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<bool> HandleLordOfLengthsomeGaitAsync()
+        {
+            BattleCharacter boss3 = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(LordOfLengthsomeGait)
+                .FirstOrDefault(bc => bc.IsTargetable && bc.Distance() < 50);
+
+            BattleCharacter liarsLyre = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(LiarsLyre)
+                .FirstOrDefault(bc => bc.CastingSpellId == Finale && bc.Distance() < 50);
+
+            if (liarsLyre != null && liarsLyre.Location.Distance2D(Core.Player.Location) >= 10.0f)
+            {
+                if (Core.Player.IsCasting)
                 {
                     ActionManager.StopCasting();
                 }
 
-                await Coroutine.Sleep(3000);
-                Logging.Write(Colors.Aquamarine, $"过独木桥");
+                Navigator.PlayerMover.MoveStop();
 
-                // 过桥
-                Vector3 location = new Vector3("-142.8355,-144.5264,-232.6624");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
+                SpellCastInfo finale = liarsLyre.SpellCastInfo;
+                TimeSpan finaleDuration = finale.RemainingCastTime;
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, finaleDuration, $"Walking tight-rope for ({finale.ActionId}) {finale.Name} for up to {finaleDuration.TotalMilliseconds:N0}ms");
+
+                foreach (Vector3 point in tightRopeWalkPoints)
                 {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
+                    Logging.Write(Colors.Aquamarine, $"Next tight-rope point: {point}");
 
-                location = new Vector3("-140.8284,-144.5366,-246.1443");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
-
-                location = new Vector3("-130.1889,-144.5366,-242.384");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
-
-                location = new Vector3("-114.455,-144.5366,-244.2632");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
-
-                location = new Vector3("-125.6857,-144.5238,-249.264");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
-
-                location = new Vector3("-122.5055,-144.5192,-258.3726");
-                while (location.Distance2D(Core.Me.Location) > 0.2)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(30);
-                }
-
-                location = new Vector3("-128.1084,-144.5226,-258.0896");
-                while (location.Distance2D(Core.Me.Location) > 1)
-                {
-                    Logging.Write(Colors.Aquamarine, $"远点距离:{location.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location);
-                    await Coroutine.Sleep(100);
-                }
-            }
-
-            if (check2.Any() == true)
-            {
-                Logging.Write(Colors.Aquamarine, $"背对");
-
-                Vector3 location1 = new Vector3("-128.474,-144.5366,-243.2417");
-                while (location1.Distance2D(Core.Me.Location) > 1)
-                {
-                    Logging.Write(Colors.Aquamarine, $"背对:{location1.Distance2D(Core.Me.Location)}");
-                    Navigator.PlayerMover.MoveTowards(location1);
-                    await Coroutine.Sleep(100);
+                    while (point.Distance2D(Core.Player.Location) > 0.2f)
+                    {
+                        Navigator.PlayerMover.MoveTowards(point);
+                        await Coroutine.Sleep(30);
+                    }
                 }
 
                 Navigator.PlayerMover.MoveStop();
-                await Coroutine.Sleep(3000);
+                CapabilityManager.Clear(CapabilityHandle, reason: $"Finished walking tight-rope for ({finale.ActionId}) {finale.Name} with {finaleDuration.TotalMilliseconds:N0}ms remaining");
             }
-
-            // 2183, 4708, 4709, 6975, 10384, 10385, 10386, 11768, 13920, 15796, 16227, 16973, 16974, 16975, 17199      :: Savage Swipe
-            // 4526, 11014, 13786, 15794, 16234                                                                         :: Unfinal Sting
-            // 5174, 5175, 13547, 13548                                                                                 :: Corrosive Bile
-            // 5176, 13549                                                                                              :: Malaise
-            // 5177, 13550                                                                                              :: Bile Bombardment
-            // 5178, 5179, 13952, 13953                                                                                 :: Flailing Tentacles
-            // 13514                                                                                                    :: Venom Shower
-            // 13551                                                                                                    :: Toad Choir
-            // 15788, 16837                                                                                             :: Pollen Corona
-            HashSet<uint> spellCastIds = new HashSet<uint>()
+            else if (boss3 != null && boss3.CastingSpellId == ImpChoir)
             {
-                2183, 4526, 4708, 4709, 5174, 5175, 5176, 5177, 5178, 5179,
-                6975, 10384, 10385, 10386, 11014, 11768, 13514, 13547, 13548,
-                13549, 13550, 13551, 13786, 13920, 13952, 13953, 15788, 15794,
-                15796, 16227, 16234, 16837, 16973, 16974, 16975, 17199,
-            };
-            bool spellCasting = GameObjectManager.GetObjectsOfType<BattleCharacter>(true, false).Where(obj =>
-                spellCastIds.Contains(obj.CastingSpellId) && obj.Distance() < 50).Count() > 0;
+                SpellCastInfo impChoir = boss3.SpellCastInfo;
+                TimeSpan gazeDuration = impChoir.RemainingCastTime + TimeSpan.FromMilliseconds(250);
 
-            if (spellCasting)
-            {
-                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, 1500, "Enemy Spell Cast In Progress");
-                await MovementHelpers.GetClosestAlly.Follow();
-            }
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, gazeDuration, $"Looking away from ({impChoir.ActionId}) {impChoir.Name} for {gazeDuration.TotalMilliseconds:N0}ms");
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Facing, gazeDuration);
+                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Targeting, gazeDuration);
 
-            if (Core.Target != null)
-            {
-                IEnumerable<BattleCharacter> sC = GameObjectManager.GetObjectsOfType<BattleCharacter>().Where(
-                    r => !r.IsMe && r.Distance() < 50 && r.NpcId == 8141);  // 73BOSS1
-                IEnumerable<BattleCharacter> sC1 = GameObjectManager.GetObjectsOfType<BattleCharacter>().Where(
-                    r => !r.IsMe && r.Distance() < 50 && r.NpcId == 8146);  // 73BOSS3
-
-                // 73boss 1 移动
-                if (sC.Any() == true)
-                {
-                    if (sidestepPlugin != null)
-                    {
-                        if (sidestepPlugin.Enabled == true)
-                        {
-                            sidestepPlugin.Enabled = false;
-                        }
-                    }
-
-                    Logging.Write(Colors.Aquamarine, $"boss1");
-                    BattleCharacter spellCaster = sC.First();
-
-                    if (spellCaster != null && spellCaster.Name == Core.Target.Name)
-                    {
-                        BattleCharacter obj = GameObjectManager.GetObjectsOfType<BattleCharacter>(true)
-                            .Where(r =>
-                              r.IsAlive &&
-                              (r.NpcId == 729 || r.NpcId == 8378 || // "雅·修特拉"
-                              r.NpcId == 1492 || // "于里昂热"
-                              r.NpcId == 4130 || // "阿尔菲诺"
-                              r.NpcId == 5239 || // "阿莉塞"
-                              r.NpcId == 8889 || // 琳
-                              r.NpcId == 11264 || // Alphinaud's avatar
-                              r.NpcId == 11265 || // Alisaie's avatar
-                              r.NpcId == 11267 || // Urianger's avatar
-                              r.NpcId == 11268 || // Y'shtola's avatar
-                              r.NpcId == 11269 || // Ryne's avatar
-                              r.NpcId == 11270 || // Estinien's avatar
-                              r.Name == "阿莉塞" ||
-                              r.Name == "琳" ||
-                              r.Name == "水晶公" ||
-                              r.Name == "敏菲利亚" ||
-                              r.Name == "桑克瑞德"))
-                            .OrderBy(r => r.Distance())
-                            .First();
-
-                        // 当距离大于跟随距离 再处理跟随
-                        if (obj.Location.Distance2D(Core.Me.Location) >= 0.2)
-                        {
-                            // 读条中断
-                            if (Core.Me.IsCasting)
-                            {
-                                ActionManager.StopCasting();
-                            }
-
-                            // 选中跟随最近的队友
-                            obj.Target();
-
-                            Logging.Write(Colors.Aquamarine, $"Following {obj.Name} Distance:{obj.Location.Distance2D(Core.Me.Location)}");
-
-                            while (obj.Location.Distance2D(Core.Me.Location) >= 0.2)
-                            {
-                                CapabilityManager.Update(CapabilityHandle, CapabilityFlags.Movement, 3000, "Avoidance In Progress");
-                                Navigator.PlayerMover.MoveTowards(obj.Location);
-                                await Coroutine.Sleep(50);
-                            }
-
-                            Navigator.PlayerMover.MoveStop();
-                            await Coroutine.Sleep(50);
-                            return true;
-                        }
-                    }
-                }
-
-                if (sC1.Any() == true)
-                {
-                    if (sidestepPlugin != null)
-                    {
-                        if (sidestepPlugin.Enabled == true)
-                        {
-                            sidestepPlugin.Enabled = false;
-                        }
-                    }
-                }
+                ActionManager.StopCasting();
+                Core.Player.ClearTarget();
+                Core.Player.FaceAway(boss3);
+                await Coroutine.Sleep(gazeDuration);
             }
 
             return false;
