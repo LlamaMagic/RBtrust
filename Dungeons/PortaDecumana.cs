67a15cbf19e96c7d50e30e40ddab1a9384d2d008
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-12-12 12:05:26 -0600

    Update PortaDecumana.cs

--- Dungeons/PortaDecumana.cs
+++ Dungeons/PortaDecumana.cs
@@ -1,6 +1,10 @@
 using Buddy.Coroutines;
+using Clio.Utilities;
+using ff14bot;
+using ff14bot.Behavior;
 using ff14bot.Managers;
 using ff14bot.Objects;
+using ff14bot.Pathing.Avoidance;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,29 +20,41 @@ namespace Trust.Dungeons;
 /// </summary>
 public class PortaDecumana : AbstractDungeon
 {
-    private const uint TheUltimaWeapon = 2137;
+    private const uint TheUltimaWeaponNpc = 2137;
+    private const int AetheroplasmNpc = 2138;
 
-    private static readonly HashSet<uint> NonSidestepSpells = new()
-    {
-        28991,
-        28999,
-        29003,
-        29011,
-        29012,
-        29013,
-        29014,
-        29021,
-    };
+    private static readonly HashSet<uint> RadiantBlaze = new() {28991};
 
-    private static readonly HashSet<uint> RadiantBlaze = new() { 28991 };
-
-    private static readonly HashSet<uint> HomingRay = new() { 29011, 29012 };
+    private static readonly HashSet<uint> HomingRay = new() {29011, 29012};
     private static readonly int HomingRayDuration = 5_000;
 
-    private static readonly HashSet<uint> CitadelBuster = new() { 29020 };
-    private static readonly HashSet<uint> LaserFocus = new() { 29013, 29014 };
+    private const uint GeocrushSpell = 28999;
+    private const uint EyeoftheStormSpell = 28980;
+    private const uint VulcanBurstSpell = 29003;
+    private const uint LaserFocusSpell = 29014;
+    private const uint CitadelBusterSpell = 29020;
+    private const uint ExplosionSpell = 29021;
+
+    DateTime EyeoftheStormDuration = DateTime.Now.AddMilliseconds(30);
+
+    private static readonly HashSet<uint> CitadelBuster = new() {29020};
+    private static readonly HashSet<uint> LaserFocus = new() {29013, 29014};
     private static readonly int CitadelBusterDuration = 5_000;
     private static DateTime citadelBusterTimestamp = DateTime.MinValue;
+
+    private static readonly Vector3 UltimaArenaCenter1 = new(-771.9428f, -400.0628f, -600.3899f);
+    private static readonly Vector3 UltimaArenaCenter2 = new(-703.6115f, -185.6595f, 479.6159f);
+
+    /*
+
+
+(2137) The Ultima Weapon, Dist: 2.79f, Loc: <-713, -185.7316, 480>, IsTargetable: False, Target: None
+  └─ Casting (29012) Homing Ray => (0) Lv. 50 Reaper
+(2137) The Ultima Weapon, Dist: 2.80f, Loc: <-713.0083, -185.7245, 479.9724>, IsTargetable: True, Target: Storm Marauder
+  └─ Casting (29011) Homing Ray => (2137) The Ultima Weapon
+  avoid other players
+
+     */
 
     /// <inheritdoc/>
     public override ZoneId ZoneId => Data.ZoneId.ThePortaDecumana;
@@ -47,60 +63,111 @@ public class PortaDecumana : AbstractDungeon
     public override DungeonId DungeonId => DungeonId.ThePortaDecumana;
 
     /// <inheritdoc/>
-    protected override HashSet<uint> SpellsToFollowDodge { get; } = new() { 28999, 29003, 29021, 29013, 29014 };
+    protected override HashSet<uint> SpellsToFollowDodge { get; } = new() { };
 
     /// <inheritdoc/>
+    public override Task<bool> OnEnterDungeonAsync()
+    {
+        AvoidanceManager.AvoidInfos.Clear();
+
+        // Ultima Titan: GeoCrush
+        AvoidanceManager.AddAvoid(new AvoidObjectInfo<BattleCharacter>(
+            condition: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: bc => bc.CastingSpellId == GeocrushSpell,
+            radiusProducer: bc => 25.0f,
+            priority: AvoidancePriority.High));
+
+        // The Ultima Weapon: Eye of the Storm
+        AvoidanceHelpers.AddAvoidDonut<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana &&
+                          DateTime.Now < EyeoftheStormDuration,
+            objectSelector: c => c.CastingSpellId == EyeoftheStormSpell,
+            outerRadius: 90.0f,
+            innerRadius: 15f,
+            priority: AvoidancePriority.Medium);
+
+        // Ultima Ifrit: Vulcan Burst
+        AvoidanceHelpers.AddAvoidDonut<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: c => c.CastingSpellId == VulcanBurstSpell,
+            outerRadius: 40.0f,
+            innerRadius: 3.0F,
+            priority: AvoidancePriority.Medium);
+
+        // Boss 1
+        // Let's avoid standing under the boss if we can help it.
+        AvoidanceManager.AddAvoid(new AvoidObjectInfo<BattleCharacter>(
+            condition: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: bc => bc.NpcId == TheUltimaWeaponNpc && bc.CanAttack,
+            radiusProducer: bc => 4.3f,
+            priority: AvoidancePriority.Medium));
+
+        // The Ultima Weapon: Citadel Buster
+        // Every time I tried to use a Rectangle here I couldn't get it to face in front of the mob. Always went off the side.
+        // So i took the easy way out and made it a cone
+        AvoidanceManager.AddAvoidUnitCone<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: (bc) => bc.CastingSpellId == CitadelBusterSpell,
+            leashPointProducer: () => UltimaArenaCenter2,
+            leashRadius: 40.0f,
+            rotationDegrees: 0.0f,
+            radius: 40.0f,
+            arcDegrees: 180.0f);
+
+        // The Ultima Weapon: Explosion
+        AvoidanceManager.AddAvoid(new AvoidObjectInfo<BattleCharacter>(
+            condition: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: bc => bc.CastingSpellId == ExplosionSpell,
+            radiusProducer: bc => 17.4f,
+            priority: AvoidancePriority.High));
+
+        /* The Ultima Weapon: Laser Focus
+         Need to make a way to set the donut around the party member that's being targetted, not the spell caster
+        AvoidanceHelpers.AddAvoidDonut<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            objectSelector: c => c.CastingSpellId == LaserFocusSpell,
+            outerRadius: 40.0f,
+            innerRadius: 3.0F,
+            priority: AvoidancePriority.Medium);
+            */
+
+        // Boss Arenas
+        AvoidanceHelpers.AddAvoidDonut(
+            () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            () => UltimaArenaCenter1,
+            outerRadius: 90.0f,
+            innerRadius: 14.0f,
+            priority: AvoidancePriority.High);
+
+        AvoidanceHelpers.AddAvoidDonut(
+            () => Core.Player.InCombat && WorldManager.ZoneId == (uint)ZoneId.ThePortaDecumana,
+            () => UltimaArenaCenter2,
+            outerRadius: 90.0f,
+            innerRadius: 19.0f,
+            priority: AvoidancePriority.High);
+
+        return Task.FromResult(false);
+    }
+
     public override async Task<bool> RunAsync()
     {
         await FollowDodgeSpells();
 
-        /*
-        [18:58:25.925 V] [SideStep] Geocrush [CastType][Id: 28999][Omen: 27][RawCastType: 2][ObjId: 1073895042]
-                Move to Ally
-        [19:03:16.528 V] [SideStep] Vulcan Burst [CastType][Id: 29003][Omen: 141][RawCastType: 2][ObjId: 1073895041]
-                Move to Ally
-        [19:04:03.848 V] [SideStep] Radiant Blaze [CastType][Id: 28991][Omen: 7][RawCastType: 2][ObjId: 1073895054]
-                Turn off Sidestep. Room wide AOE that you can only dodge by DPSing the boss.
-        [15:22:37.051 V] [SideStep] Weight of the Land [CastType][Id: 29001][Omen: 8][RawCastType: 2][ObjId: 1073749701]
-                Sidestep handles
-         [15:38:39.100 V] [SideStep] Magitek Ray [CastType][Id: 29008][Omen: 26][RawCastType: 12][ObjId: 1073751790]
-                Sidestep handles
-                [15:34:54.855 V] [SideStep] Assault Cannon [CastType][Id: 29019][Omen: 2][RawCastType: 12][ObjId: 1073751675]
-                        Sidestep Handles
-                [15:35:24.502 V] [SideStep] Explosion [CastType][Id: 29021][Omen: 27][RawCastType: 2][ObjId: 1073750976]
-                    Move To alley
-
-        Laser Focus 29013, 29014
-
-        Citadel Buster 29020
-            Move away from the front of the boss
-
-         */
-
-        // Ultima Weapon
-        // Radiant Blaze [CastType][Id: 28991][Omen: 7][RawCastType: 2][ObjId: 1073895054]
-        if (RadiantBlaze.IsCasting())
+        if (LaserFocus.IsCasting())
         {
-            SidestepPlugin.Enabled = false;
+            BattleCharacter laserFocusTarget = PartyManager.AllMembers
+                .Select(pm => pm.BattleCharacter)
+                .OrderBy(obj => obj.Distance(Core.Player))
+                .FirstOrDefault(obj => !obj.IsMe);
+
+            await CommonTasks.MoveTo(laserFocusTarget.Location);
+            await Coroutine.Sleep(30);
         }
 
         if (HomingRay.IsCasting())
         {
-            SidestepPlugin.Enabled = false;
-            AvoidanceManager.RemoveAllAvoids(i => i.CanRun);
             await MovementHelpers.Spread(HomingRayDuration);
         }
-
-        if (CitadelBuster.IsCasting() && citadelBusterTimestamp < DateTime.Now)
-        {
-            BattleCharacter ultimaWeaponNpc = GameObjectManager.GetObjectsByNPCId<BattleCharacter>(TheUltimaWeapon)
-                .FirstOrDefault(bc => bc.IsTargetable);
-            SidestepPlugin.Enabled = true;
-            AvoidanceHelpers.AddAvoidRectangle(ultimaWeaponNpc, 12.0f, 40.0f);
-            citadelBusterTimestamp = DateTime.Now.AddMilliseconds(CitadelBusterDuration);
-        }
-
-        SidestepPlugin.Enabled = !NonSidestepSpells.IsCasting();
 
         await Coroutine.Yield();
 
