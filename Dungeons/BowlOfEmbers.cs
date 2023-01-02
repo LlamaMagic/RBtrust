599f688897503e7eb696ad58b91366e3f20f76e1
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-01-01 19:10:08 -0600

    Update BowlOfEmbers.cs
    Added avoid for non-tanks

--- Dungeons/BowlOfEmbers.cs
+++ Dungeons/BowlOfEmbers.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using Clio.Utilities;
+using ff14bot;
+using ff14bot.Managers;
+using ff14bot.Objects;
+using ff14bot.Pathing.Avoidance;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Trust.Data;
+using Trust.Extensions;
+using Trust.Helpers;
 
 namespace Trust.Dungeons;
 
@@ -9,6 +16,10 @@ namespace Trust.Dungeons;
 /// </summary>
 public class BowlOfEmbers : AbstractDungeon
 {
+    private const int IfritNPCID = 1185;
+
+    private static readonly Vector3 IfritArenaCenter = new(2.016229f, 0f, 1.375818f);
+
     /// <inheritdoc/>
     public override ZoneId ZoneId => Data.ZoneId.TheBowlOfEmbers;
 
@@ -17,6 +28,25 @@ public class BowlOfEmbers : AbstractDungeon
 
     /// <inheritdoc/>
     protected override HashSet<uint> SpellsToFollowDodge { get; } = null;
+
+    public override Task<bool> OnEnterDungeonAsync()
+    {
+        AvoidanceManager.AvoidInfos.Clear();
+
+        // Boss 1
+        // In general, if not tank stay out of the front to avoid AOE breath attack
+        AvoidanceManager.AddAvoidUnitCone<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.ZoneId == 1045 &&
+                          !Core.Me.IsTank(),
+            objectSelector: (bc) => bc.NpcId == IfritNPCID && bc.CanAttack,
+            leashPointProducer: () => IfritArenaCenter,
+            leashRadius: 40.0f,
+            rotationDegrees: 0.0f,
+            radius: 15.0f,
+            arcDegrees: 160.0f);
+
+        return Task.FromResult(false);
+    }
 
     /// <inheritdoc/>
     public override async Task<bool> RunAsync()
