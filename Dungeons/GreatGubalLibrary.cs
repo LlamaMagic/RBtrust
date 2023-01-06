4b9528b0ba85523da8fd09f2ec09d735d00b0dea
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-01-06 10:30:59 -0600

    Update GreatGubalLibrary.cs
    Added room centers, boss IDs, and boss arena avoids

--- Dungeons/GreatGubalLibrary.cs
+++ Dungeons/GreatGubalLibrary.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Clio.Utilities;
+using ff14bot;
+using ff14bot.Managers;
+using ff14bot.Pathing.Avoidance;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Trust.Data;
+using Trust.Helpers;
 
 namespace Trust.Dungeons;
 
@@ -9,6 +14,14 @@ namespace Trust.Dungeons;
 /// </summary>
 public class GreatGubalLibrary : AbstractDungeon
 {
+    private const int DemonTomeNPCID = 3923;
+    private const int ByblosNPCID = 3925;
+    private const int TheEverlivingBibliotaphNPCID = 3930;
+
+    private static readonly Vector3 DemonTomeArenaCenter = new(0f, 0f, 0f);
+    private static readonly Vector3 ByblosArenaCenter = new(177.7828f, -8f, 27.11523f);
+    private static readonly Vector3 TheEverlivingBibliotaphArenaCenter = new(377.7593f, -39f, -59.76191f);
+
     /// <inheritdoc/>
     public override ZoneId ZoneId => Data.ZoneId.TheGreatGubalLibrary;
 
@@ -17,6 +30,35 @@ public class GreatGubalLibrary : AbstractDungeon
 
     /// <inheritdoc/>
     protected override HashSet<uint> SpellsToFollowDodge { get; } = null;
+
+    public override Task<bool> OnEnterDungeonAsync()
+    {
+        AvoidanceManager.AvoidInfos.Clear();
+
+        // Boss Arenas
+        AvoidanceHelpers.AddAvoidDonut(
+            () => Core.Player.InCombat && WorldManager.SubZoneId == (uint)SubZoneId.HallofMagicks,
+            () => DemonTomeArenaCenter,
+            outerRadius: 90.0f,
+            innerRadius: 19.0f,
+            priority: AvoidancePriority.High);
+
+        AvoidanceHelpers.AddAvoidDonut(
+            () => Core.Player.InCombat && WorldManager.SubZoneId == (uint)SubZoneId.AstrologyandAstromancyCamera,
+            () => ByblosArenaCenter,
+            outerRadius: 90.0f,
+            innerRadius: 19.5f,
+            priority: AvoidancePriority.High);
+
+        AvoidanceHelpers.AddAvoidDonut(
+            () => Core.Player.InCombat && WorldManager.SubZoneId == (uint)SubZoneId.RhapsodiesQuadrangle,
+            () => TheEverlivingBibliotaphArenaCenter,
+            outerRadius: 90.0f,
+            innerRadius: 19.0f,
+            priority: AvoidancePriority.High);
+
+        return Task.FromResult(false);
+    }
 
     /// <inheritdoc/>
     public override async Task<bool> RunAsync()
