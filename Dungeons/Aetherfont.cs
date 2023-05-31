e94efa25d56e27cc81932c43d2011904a6a16507
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-05-31 17:01:12 -0500

    Update Aetherfont.cs
    Tweaked mechanics a bit

--- Dungeons/Aetherfont.cs
+++ Dungeons/Aetherfont.cs
@@ -1,6 +1,7 @@
 ﻿using Clio.Utilities;
 using ff14bot;
 using ff14bot.Managers;
+using ff14bot.Objects;
 using ff14bot.Pathing.Avoidance;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -22,11 +23,19 @@ public class Aetherfont : AbstractDungeon
     public override DungeonId DungeonId => DungeonId.Aetherfont;
 
     /// <inheritdoc/>
-    protected override HashSet<uint> SpellsToFollowDodge { get; } = new() { EnemyAction.LightningClaw, EnemyAction.Tidalspout };
+    protected override HashSet<uint> SpellsToFollowDodge { get; } = new() { EnemyAction.Clearout, EnemyAction.ExplosiveFrequency, EnemyAction.ResonantFrequency, EnemyAction.LightningClaw, EnemyAction.Tidalspout, EnemyAction.LightningRampage };
 
     public override Task<bool> OnEnterDungeonAsync()
     {
         AvoidanceManager.AvoidInfos.Clear();
+
+        // Boss 2 Forked Fissures
+        AvoidanceHelpers.AddAvoidRectangle<BattleCharacter>(
+            canRun: () => Core.Player.InCombat && WorldManager.SubZoneId == (uint)SubZoneId.CyancapCavern,
+            objectSelector: bc => bc.CastingSpellId == EnemyAction.ForkedFissures,
+            width: 3.5f,
+            length: 40f,
+            priority: AvoidancePriority.High);
 
         // Boss Arenas
 
@@ -41,7 +50,7 @@ public class Aetherfont : AbstractDungeon
             () => Core.Player.InCombat && WorldManager.SubZoneId == (uint)SubZoneId.CyancapCavern,
             () => ArenaCenter.Arkas,
             outerRadius: 90.0f,
-            innerRadius: 19.0f,
+            innerRadius: 10.0f,
             priority: AvoidancePriority.High);
 /*
         AvoidanceHelpers.AddAvoidDonut(
@@ -63,6 +72,11 @@ public class Aetherfont : AbstractDungeon
         if (EnemyAction.Waterspout.IsCasting())
         {
             await MovementHelpers.Spread(EnemyAction.WaterspoutDuration);
+        }
+
+        if (EnemyAction.WaterDrop.IsCasting())
+        {
+            await MovementHelpers.Spread(EnemyAction.WaterDropDuration);
         }
 
         return false;
@@ -117,6 +131,20 @@ public class Aetherfont : AbstractDungeon
 
         /// <summary>
         /// Lyngbakr
+        /// Explosive Frequency
+        /// Adding these to follow as when they happen at the same time it confuses RB
+        /// </summary>
+        public const uint ExplosiveFrequency = 33340;
+        /// <summary>
+        /// Lyngbakr
+        /// Resonant Frequency
+        /// Adding these to follow as when they happen at the same time it confuses RB
+        /// </summary>
+        public const uint ResonantFrequency = 33339;
+
+
+        /// <summary>
+        /// Lyngbakr
         /// Tidalspout
         /// Stack
         /// </summary>
@@ -128,5 +156,35 @@ public class Aetherfont : AbstractDungeon
         /// Stack
         /// </summary>
         public const uint LightningClaw = 34712;
+
+        /// <summary>
+        /// Varshahn
+        /// Lightning Rampage
+        ///
+        /// </summary>
+        public const uint LightningRampage = 34319;
+
+        /// <summary>
+        /// Varshahn
+        /// Forked Fissures
+        ///
+        /// </summary>
+        public const uint ForkedFissures = 33361;
+
+        /// <summary>
+        /// Octomammoth
+        /// Water Drop
+        /// Spread
+        /// </summary>
+        public static readonly HashSet<uint> WaterDrop = new() { 34436 };
+
+        public static readonly int WaterDropDuration = 5_000;
+
+        /// <summary>
+        /// Octomammoth
+        /// Clearout
+        /// Adding follow on this one as the avoids are so pinpoint that the nav gets stuck trying to get out of the AoE
+        /// </summary>
+        public const uint Clearout = 33348;
     }
 }
