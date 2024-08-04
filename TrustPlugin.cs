aa6b0bbf2923706954009644b98a4bea537ca3ec
domesticwarlord86 <domesticwarlord86@gmail.com>
2024-08-04 15:47:59 -0500

    fix: Updated plugin version number

--- TrustPlugin.cs
+++ TrustPlugin.cs
@@ -32,7 +32,7 @@ public class TrustPlugin : BotPlugin
     public override string Name => Translations.PROJECT_NAME;
 
     /// <inheritdoc/>
-    public override Version Version => new(1, 2, 3);
+    public override Version Version => new(7, 0, 04082024);
 
     /// <inheritdoc/>
     public override bool WantButton => false;
